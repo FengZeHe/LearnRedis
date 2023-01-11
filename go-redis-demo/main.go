e97ddad2324dfc831e5c0185d82a2c10dc619822
feng <hzf1998@126.com>
2023-01-11 10:54:11 +0800

    add go-redis example

---
+++ go-redis-demo/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+)
+
+var ctx = context.Background()
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "baykee0757",
+		DB:       0,
+	})
+
+	_, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Successful connection")
+	}
+
+	err = rdb.Set(ctx, "Jim", "man", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "Jim").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(val)
+
+}
